fde2b428d6d35cd7cd822fc1a944f380ec6b9695
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-06-30 11:37:28 -0700

    test is green with 100% line coverage. Can now calculate age on venus.

--- src/galaticAge.js
+++ src/galaticAge.js
@@ -3,4 +3,17 @@ export default class superAge {
   constructor(age) {
     this.age = age;
   }
+
+  planetAge(planet) {
+    let mercury;
+    let venus;
+    switch(planet) {
+      case "mercury":
+        mercury = parseFloat((this.age/0.24).toFixed(1));
+        return mercury;
+      case "venus":
+        venus = parseFloat((this.age/0.62).toFixed(1));
+        return venus;
+    }
+  }
 }
