a838645027d316e69d915ad755c630e72886c732
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-07-01 11:10:44 -0700

    red test changed the ages and expected to see if it would still work correctly. Changed the first test to a different expectedAge and it failed.

--- __tests__/galacticAge.test.js
+++ __tests__/galacticAge.test.js
@@ -3,59 +3,59 @@ import superAge from "../src/galacticAge";
 describe ('superAge', () => {
   let calculate;
   beforeEach(() => {
-    calculate = new superAge(30, 80)
+    calculate = new superAge(20, 97.3)
   });
 
   test(`Should be able to calculate the users current Earth age`,() => {
-    expect(calculate.age).toBe(30);
-    expect(calculate.expectedAge).toBe(80)
+    expect(calculate.age).toBe(20);
+    expect(calculate.expectedAge).toBe(99.5)
   });
 
   test(`Should be able to calculate the users current age on Mercury`, () => {
-    expect(calculate.planetAge("mercury")).toBe(125);
+    expect(calculate.planetAge("mercury")).toBe(83.3);
   });
 
   test(`Should be able to calculate the users current age on Venus`, () => {
-    expect(calculate.planetAge("venus")).toBe(48.4);
+    expect(calculate.planetAge("venus")).toBe(32.3);
   });
 
   test(`Should be able to calculate the users current age on Mars`, () => {
-    expect(calculate.planetAge("mars")).toBe(16);
+    expect(calculate.planetAge("mars")).toBe(10.6);
   });
 
   test(`Should be able to calculate the users current age on Jupiter`, () => {
-    expect(calculate.planetAge("jupiter")).toBe(2.5);
+    expect(calculate.planetAge("jupiter")).toBe(1.7);
   });
 
   test(`Should be able to calculate the users remaining age on Mercury`, () => {
-    expect(calculate.expectedLife("mercury")).toBe(333.3);
+    expect(calculate.expectedLife("mercury")).toBe(405.4);
   });
 
   test(`Should be able to calculate the users remaining age on Venus`, () => {
-    expect(calculate.expectedLife("venus")).toBe(129)
+    expect(calculate.expectedLife("venus")).toBe(156.9)
   });
 
   test(`Should be able to calculate the users remaining age on Mars`, () => {
-    expect(calculate.expectedLife("mars")).toBe(42.6)
+    expect(calculate.expectedLife("mars")).toBe(51.8)
   });
 
   test(`Should be able to calculate the users remaining age on Jupiter`, () => {
-    expect(calculate.expectedLife("jupiter")).toBe(6.7)
+    expect(calculate.expectedLife("jupiter")).toBe(8.2)
   });
 
   test(`Should be able to calculate the users remaining time on Mercury`, () => {
-    expect(calculate.remainingLife("mercury")).toBe(208.3)
+    expect(calculate.remainingLife("mercury")).toBe(322.1)
   });
 
   test(`Should be able to calculate the users remaining time on Venus`, () => {
-    expect(calculate.remainingLife("venus")).toBe(80.6)
+    expect(calculate.remainingLife("venus")).toBe(124.7)
   });
 
   test(`Should be able to calculate the users remaining time on Mars`, () => {
-    expect(calculate.remainingLife("mars")).toBe(26.6)
+    expect(calculate.remainingLife("mars")).toBe(41.1)
   });
 
   test(`Should be able to calculate the users remaining time on Jupiter`, () => {
-    expect(calculate.remainingLife("jupiter")).toBe(4.2)
+    expect(calculate.remainingLife("jupiter")).toBe(6.5)
   });
 });
