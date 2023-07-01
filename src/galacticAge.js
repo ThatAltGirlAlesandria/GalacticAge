afa324a0c36aa60f58c6e407a15ff9bc2a05a972
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-07-01 14:34:01 -0700

    fixed all errors and warnings from ESLint.

--- src/galacticAge.js
+++ src/galacticAge.js
@@ -4,7 +4,7 @@ export default class superAge {
     this.age = age;
     this.expectedAge = expectedAge;
     this.expectedExpiration = expectedAge - age;
-  };
+  }
 
   planetAge(planet) {
     let mercury;
@@ -12,20 +12,20 @@ export default class superAge {
     let mars;
     let jupiter;
     switch(planet) {
-      case "mercury":
-        mercury = parseFloat((this.age/0.24).toFixed(1));
-        return mercury;
-      case "venus":
-        venus = parseFloat((this.age/0.62).toFixed(1));
-        return venus;
-      case "mars":
-        mars = parseFloat((this.age/1.88).toFixed(1));
-        return mars;
-      case "jupiter":
-        jupiter = parseFloat((this.age/11.86).toFixed(1));
-        return jupiter
-    };
-  };
+    case "mercury":
+      mercury = parseFloat((this.age/.24).toFixed(1));
+      return mercury;
+    case "venus":
+      venus = parseFloat((this.age/.62).toFixed(1));
+      return venus;
+    case "mars":
+      mars = parseFloat((this.age/1.88).toFixed(1));
+      return mars;
+    case "jupiter":
+      jupiter = parseFloat((this.age/11.86).toFixed(1));
+      return jupiter;
+    }
+  }
 
   expectedLife(planet) {
     let expectedMercury;
@@ -33,20 +33,19 @@ export default class superAge {
     let expectedMars;
     let expectedJupiter;
     switch(planet) {
-      case "mercury":
-        expectedMercury = parseFloat((this.expectedAge/0.24).toFixed(1));
-        return expectedMercury;
-      case "venus":
-        expectedVenus = parseFloat((this.expectedAge/0.62).toFixed(1));
-        return expectedVenus;
-      case "mars":
-        expectedMars = parseFloat((this.expectedAge/1.88).toFixed(1));
-        return expectedMars;
-      case "jupiter":
-        expectedJupiter = parseFloat((this.expectedAge/11.86).toFixed(1));
-        return expectedJupiter;
-      };
-  };
+    case "mercury":
+      expectedMercury = parseFloat((this.expectedAge/.24).toFixed(1));
+      return expectedMercury;
+    case "venus":
+      expectedVenus = parseFloat((this.expectedAge/.62).toFixed(1));
+      return expectedVenus;
+    case "mars":
+      expectedMars = parseFloat((this.expectedAge/1.88).toFixed(1));
+      return expectedMars;
+    case "jupiter":
+      expectedJupiter = parseFloat((this.expectedAge/11.86).toFixed(1));        return expectedJupiter;
+    }
+  }
 
   remainingLife(planet) {
     let remainingMercury;
@@ -54,18 +53,18 @@ export default class superAge {
     let remainingMars;
     let remainingJupiter;
     switch(planet) {
-      case "mercury":
-        remainingMercury = parseFloat((this.expectedExpiration/0.24).toFixed(1));
-        return remainingMercury;
-      case "venus":
-        remainingVenus = parseFloat((this.expectedExpiration/0.62).toFixed(1));
-        return remainingVenus;
-      case "mars":
-        remainingMars = parseFloat((this.expectedExpiration/1.88).toFixed(1));
-        return remainingMars;
-      case "jupiter":
-        remainingJupiter = parseFloat((this.expectedExpiration/11.86).toFixed(1));
-        return remainingJupiter;
-    };
-  };
+    case "mercury":
+      remainingMercury = parseFloat((this.expectedExpiration/.24).toFixed(1));
+      return remainingMercury;
+    case "venus":
+      remainingVenus = parseFloat((this.expectedExpiration/.62).toFixed(1));
+      return remainingVenus;
+    case "mars":
+      remainingMars = parseFloat((this.expectedExpiration/1.88).toFixed(1));
+      return remainingMars;
+    case "jupiter":
+      remainingJupiter = parseFloat((this.expectedExpiration/11.86).toFixed(1));
+      return remainingJupiter;
+    }
+  }
 }
